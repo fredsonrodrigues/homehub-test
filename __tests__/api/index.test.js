8e56affcba1057e34e09e3c82ce2e5bb89a348b1
Fredson Rodrigues <fredson.rodrigues@luizalabs.com>
2021-02-22 17:55:59 -0300

    test: add tests configs and api tests

---
+++ __tests__/api/index.test.js
@@ -0,0 +1,58 @@
+import { createMocks } from 'node-mocks-http';
+import getBreeds from '../../pages/api/breeds/index.js';
+import getOneBreed from '../../pages/api/breeds/[pid].js';
+
+describe('testing API /breeds/', () => {
+
+  const setup = async (props) => {
+    const { req, res } = createMocks({
+      method: 'GET',
+      ...props
+    });
+    await getBreeds(req, res);
+    return {
+      req,
+      res
+    }
+  }
+
+  const setupOneBreed = async (props) => {
+    const { req, res } = createMocks({
+      method: 'GET',
+      ...props
+    });
+    await getOneBreed(req, res);
+    return {
+      req,
+      res
+    }
+  }
+
+  test('returns a correctly breeds length', async () => {
+    const { req, res } = await setup();
+    expect(res._getStatusCode()).toBe(200);
+    expect(JSON.parse(res._getData())).toHaveLength(4);
+  });
+
+  test('returns the first specified breed correctly', async () => {
+    const { req, res } = await setup();
+    expect(res._getStatusCode()).toBe(200);
+    expect(JSON.parse(res._getData())[0]).toEqual(
+      expect.objectContaining({
+        name: "Akita",
+      }),
+    );
+  });
+
+  test('returns the selected breed correctly', async () => {
+    const { req, res } = await setupOneBreed({ query: { pid: 121 }});
+    expect(res._getStatusCode()).toBe(200);
+    expect(JSON.parse(res._getData())).toEqual(
+      expect.objectContaining({
+        name: "Golden Retriever",
+        temperament: "Intelligent, Kind, Reliable, Friendly, Trustworthy, Confident"
+      }),
+    );
+  });
+
+});
